46b5391d47a83c3da0082c118d1be14d5f28256c
miloetz <111991053+miloetz@users.noreply.github.com>
2026-03-03 16:08:21 -0600

    compilation error fix

--- config/dev.exs
+++ config/dev.exs
@@ -57,13 +57,9 @@ config :thalassaxir, ThalassaxirWeb.Endpoint,
   live_reload: [
     web_console_logger: true,
     patterns: [
-      # Static assets, except user uploads
-      ~r"priv/static/(?!uploads/).*\.(js|css|png|jpeg|jpg|gif|svg)$"E,
-      # Gettext translations
-      ~r"priv/gettext/.*\.po$"E,
-      # Router, Controllers, LiveViews and LiveComponents
-      ~r"lib/thalassaxir_web/router\.ex$"E,
-      ~r"lib/thalassaxir_web/(controllers|live|components)/.*\.(ex|heex)$"E
+      ~r"priv/static/(?!uploads/).*(js|css|png|jpeg|jpg|gif|svg)$",
+      ~r"priv/gettext/.*.po$",
+      ~r"lib/thalassaxir_web/(controllers|live|components)/.*(ex|heex)$"
     ]
   ]
 
